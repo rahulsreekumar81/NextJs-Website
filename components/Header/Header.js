9e88db7623edc9f846b9ff0fa8e9e196937fb7ca
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-02 12:42:48 +0530

    ✅ Added Header Skelton

---
+++ components/Header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <nav className="nav__container">
+      <div className="nav__logo">LOGO</div>
+      <ul className="nav__links">Home</ul>
+      <ul className="nav__links">Explore</ul>
+      <div className="nav__actions">
+        <button>Get Started</button>
+      </div>
+    </nav>
+  );
+};
+export default Header;
