9677b07722ce798bdfc813c058ba174196339fe6
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-03 13:11:54 +0530

    ♻️ : Refactored Header Skeleton

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,14 +1,27 @@
 import Button from "../Button/Button";
+import Image from "next/image";
 const Header = () => {
   return (
-    <nav className="nav__container">
-      <div className="nav__logo">LOGO</div>
-      <ul className="nav__links">Home</ul>
-      <ul className="nav__links">Explore</ul>
-      <div className="nav__actions">
-        <Button button_title="Get Started" />
-      </div>
-    </nav>
+    <header>
+      <Image src="/assets\images\LOGO.png" alt="logo" layout="fill" />
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__list-item">
+            <a href="" className="nav__link">
+              Home
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="" className="nav__link">
+              Explore
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <Button button_text="Get Started" />
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 export default Header;
