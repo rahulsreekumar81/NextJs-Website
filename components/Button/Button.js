f7072c3e0538dd8d0dac9d95d273bdae229c1b77
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-07 12:32:30 +0530

    💄 : Added Button and Button Style file

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,5 +1,6 @@
+import styles from "../../styles/Button.module.css";
 const Button = ({ button_text, icon }) => {
-  return <button className="button">{button_text}</button>;
+  return <button className={styles.button}>{button_text}</button>;
 };
 
 export default Button;
