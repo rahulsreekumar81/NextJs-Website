3656620301215cb152be628e44e649764ff27fc8
rahulsreekumar81 <rahulsreekumar81@gmail.com>
2022-07-07 12:33:25 +0530

    ♻️ : Refactored Header & CSS file

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,22 +1,24 @@
+import styles from "../../styles/Header.module.css";
 import Button from "../Button/Button";
 import Image from "next/image";
+import LOGO from "/assets/images/LOGO.png";
 const Header = () => {
   return (
-    <header>
-      <Image src="/assets\images\LOGO.png" alt="logo" layout="fill" />
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__list-item">
-            <a href="" className="nav__link">
+    <header className={styles.header}>
+      <Image src={LOGO} alt="logo" height={100} width={100} />
+      <nav className={styles.nav}>
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__listitem}>
+            <a href="#" className="nav__link">
               Home
             </a>
           </li>
-          <li className="nav__list-item">
-            <a href="" className="nav__link">
+          <li className={styles.nav__listitem}>
+            <a href="#" className={styles.nav__link}>
               Explore
             </a>
           </li>
-          <li className="nav__list-item">
+          <li className={styles.nav__listitem}>
             <Button button_text="Get Started" />
           </li>
         </ul>
